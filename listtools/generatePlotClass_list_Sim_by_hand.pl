92033fd53c37f8271947aa7dd12a39027f342c1a
deividribeiro <deividlsribeiro@gmail.com>
2016-03-04 17:09:04 -0500

    This updates the single script for the vegasAnalysis (by hand) but fixes some of the extra declarations. Should have been part of the previous commit.

--- listtools/generatePlotClass_list_Sim_by_hand.pl
+++ listtools/generatePlotClass_list_Sim_by_hand.pl
@@ -40,35 +40,10 @@ foreach $epoch (@epochs)
           @druns = ( @druns, $dataRun );
           @fruns = ( @fruns, $flasherRun );
 ($str = qq{
-char epo='$epoch';
-int epochs = 6;
-int wobbles = $wobble;
-int atms=$atm;
-int Zeniths=$zenith;
-int noises=$noise;
-char runlistdir[200] = "/a/home/tehanu/dribeiro/Analysis/CrabSim/runlists/Classes/";
-char cuts[10] = "med-com";
-char suffix[10] = "$suffix";
-gROOT->ProcessLine(".L /a/home/tehanu/omw2107/classes/vegasAnalysis.cpp");
-vector <float> bin;
-vector <float> res;
-vector <float> rms;
-char results_table[100];
-sprintf(results_table,"%sResults_%s.txt",runlistdir,suffix);
-FILE * file = fopen(results_table,"a+");
-char Error_table[100];
-sprintf(Error_table,"%sErrors_%s.txt",runlistdir,suffix);
-FILE * efile = fopen(Error_table,"w");
-string txt[300];
-string line;
-int bins = 10;
-string dataRun;
-string runlist_file;
 vegasAnalysis *v = new vegasAnalysis("$runparamscript",999999);
 v->getBinnedResolution(bin, res, rms, "fEnergy_GeV", 0, bins,"Logarithmic");
 cout << "DATATATA"<< endl;
 for(int i=0;i<bins;i++) cout << epo << " " << atms << " " << Zeniths << " " <<  wobbles << " " <<  noises << " " <<  bin[i] << " " << res[i] << " " <<  rms[i] <<  endl;
-gROOT->Reset();
 });
 print $str;
 
